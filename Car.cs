19f970fec80cf812945127094628e0e605c496c6
ar5wa <arwarashedalkh@outlook.sa>
2024-12-26 14:44:06 +0300

    finshed with the methods

---
+++ Car.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Car: Vehicle{
+ public override void Start(){
+        Console.WriteLine("the car is starting with smooth engine sound !");
+}
+public string FuelEfficinecy{get; set;}
+}
